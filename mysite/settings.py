c3bbb31fcd6ed1ed1d7f0cd05c79724e29374ed9
Marina <marinamsm@live.com>
2016-12-18 14:32:20 -0200

    permissao para hospedar blog pelo pythonanywhere

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5l7_85n%+pfibq&fqsfeimm8#f=rus1su4hwwkbsvr&f1@3y#b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'marinamsm.pythonanywhere.com']
 
 
 # Application definition
